f0e6638d1ea25173ba5950dbb060756de50b70ba
FIMehedi <fokrulislammehedi2000@gmail.com>
2020-03-03 13:12:01 +0600

    let & const Done!

---
+++ letconst.js
@@ -0,0 +1,42 @@
+const name = "F I Mehedi";
+console.log(name);
+
+const numbers = [21, 43, 65];
+console.log(numbers);
+
+numbers[1] = 45;
+console.log(numbers);
+
+numbers.push(55);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+const obj = {
+    name : "F I Mehedi",
+    roll : 168615,
+    reg : 1500978832
+}
+console.log(obj);
+
+console.log(obj.name);
+
+obj.name = "Fokrul Islam Mehedi";
+console.log(obj.name);
+
+
+let studentName = "Mehedi";
+studentName = "Fokrul Islam Mehedi"
+console.log(studentName);
+
+
+const friendName = ["Mehedi", "Masum", "Riad"];
+
+for (let i = 0; i < friendName.length; i++) {
+    const element = friendName[i];
+    console.log(element);
+};
+
+
+
