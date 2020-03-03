e96cccd8a70ca5798c94d8903ce1e7ed793cc4f8
FIMehedi <fokrulislammehedi2000@gmail.com>
2020-03-03 20:20:09 +0600

    basic class done!

---
+++ class.js
@@ -0,0 +1,16 @@
+class Student{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.college = "Lakshmipur Polytechnic Institute"
+    }
+};
+const student1 = new Student(1, "Fokrul Islam");
+const student2 = new Student(2, "Afsar Uddin");
+
+
+console.log(student1);
+console.log(student2);
+
+console.log(student1.name);
+
